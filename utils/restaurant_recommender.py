144deefc52c69c7e6f52cc8536127858fbc98112
Chua Hao Zi <67159970+hzchua@users.noreply.github.com>
2022-11-03 15:58:58 +0800

    Add files via upload

--- utils/restaurant_recommender.py
+++ utils/restaurant_recommender.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import pickle
+import numpy as np
 
-def res_rec(userID="110357160800000000000"):
+def res_rec(userID=1.002151589e+20):
   ## Use default userID
   df1=pd.read_excel('data/df1.xlsx')
+  similarity_matrix = np.load("models/similarity_matrix.npy")
+  with open('models/mappingTable.pkl', 'rb') as file:
+    mapping = pickle.load(file)  
+    
   bestSent=df1.sort_values(['rating'],ascending=False).groupby(['gPlusUserId'],as_index=False)[['gPlusUserId','name','rating']].first()
   restaurantChosen=bestSent["name"].iloc[bestSent.index[bestSent["gPlusUserId"]== userID]].values[0]
   restaurant_index = mapping[restaurantChosen]
@@ -12,16 +17,9 @@ def res_rec(userID="110357160800000000000"):
   similarity_score = list(enumerate(similarity_matrix[restaurant_index]))
   #sort in descending order the similarity score of movie inputted with all the other movies
   similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-  # Get the scores of the 15 most similar movies. Ignore the first movie.
+  # Get the scores of the 5 most similar movies. Ignore the first movie.
   similarity_score = similarity_score[1:4]
   #return movie names using the mapping series
   restaurant_index = [i[0] for i in similarity_score]
-  top3_recommendation =  (pd.DataFrame({'Name':df1['name'].iloc[restaurant_index],'Categories':df1['categories'].iloc[restaurant_index]}))
-  return top3_recommendation
-
-if __name__ == "__main__":
-  with open('models/similarityMatrix.pkl') as pickle_file:
-    content = pickle.load(pickle_file)
-  # df1 = pickle.load('models/similarityMatrix.pkl')
-  # df1.to_excel("similarityMatrix.xlsx", index=False)
-  print(content)
+  # top3_recommendation =  (pd.DataFrame({'Name':df1['name'].iloc[restaurant_index],'Categories':df1['categories'].iloc[restaurant_index]}))
+  return "Top 3 restaurants:\n1) "+str(df1['name'].iloc[restaurant_index].values[0])+"\n2) "+str(df1['name'].iloc[restaurant_index].values[1])+"\n3) "+str(df1['name'].iloc[restaurant_index].values[2])
